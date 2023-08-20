06cd9d750bb84e4782795eeecaf2f2c1dd501199
Anurag Dhote <anuragdhote392@gmail.com>
2023-08-20 17:06:20 +0530

    feat - sync with document with same document ids

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const io = require('socket.io')(3001, {
 });
 
 io.on('connection', socket => {
-
-    socket.on('send-changes', delta => {
-        // console.log(delta)
-        socket.broadcast.emit('receive-changes', delta)
-    })
-
     socket.on('get-document', documentId => {
         console.log('connected with id : ' + documentId) 
+        const data = ''
+        socket.join(documentId)
+        socket.emit('load-document', data)
+
+        socket.on('send-changes', delta => {
+            // console.log(delta)
+            socket.broadcast.to(documentId).emit('receive-changes', delta)
+        })
     })
 });
