3e4e86b6ca1d4518f278d5a0419b4b6ac557aabc
Anurag Dhote <anuragdhote392@gmail.com>
2023-08-20 17:22:58 +0530

    feat - fetching and saving to db document

---
+++ db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectToDB = () => {
+    mongoose
+        .connect('mongodb://127.0.0.1:27017/google-docs-clone')
+        .then(() => console.log('Connected to MongoDB'))
+        .catch(err => console.log(err.message));
+}
+
+module.exports = connectToDB;
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const connectToDB = require('./db/mongoose.js')
+const Document = require('./models/Document.js')
+
+connectToDB()
+const defaultValue = ''
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
@@ -6,15 +12,27 @@ const io = require('socket.io')(3001, {
 });
 
 io.on('connection', socket => {
-    socket.on('get-document', documentId => {
+    socket.on('get-document', async (documentId) => {
         console.log('connected with id : ' + documentId) 
-        const data = ''
+        const document = await findOrCreateDocument(documentId)
         socket.join(documentId)
-        socket.emit('load-document', data)
+        socket.emit('load-document', document.data)
 
         socket.on('send-changes', delta => {
             // console.log(delta)
             socket.broadcast.to(documentId).emit('receive-changes', delta)
         })
+
+        socket.on('save-changes', async (data) => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        })
     })
 });
+
+const findOrCreateDocument = async (id) => {
+    if (id == null) return
+
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({ _id: id, data: defaultValue })
+}
