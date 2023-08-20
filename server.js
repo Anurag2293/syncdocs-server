b592f91c0eecd1a818f6cd488fea2c8d289cfce3
Anurag Dhote <anuragdhote392@gmail.com>
2023-08-20 17:01:36 +0530

    initial commit with send and receive changes for document writes

---
+++ server.js
@@ -0,0 +1,18 @@
+const io = require('socket.io')(3001, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', socket => {
+
+    socket.on('send-changes', delta => {
+        // console.log(delta)
+        socket.broadcast.emit('receive-changes', delta)
+    })
+
+    socket.on('get-document', documentId => {
+        console.log('connected with id : ' + documentId) 
+    })
+});
